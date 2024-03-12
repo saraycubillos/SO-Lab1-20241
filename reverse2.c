2751c3750a7eab8798ba42f85f7f1e55732dd03c
Mgll3 <miguel.alvarez1@udea.du.co>
2024-03-11 19:25:51 -0500

    Co-authored-by: saraycubillos <saraycubillos@users.noreply.github.com> Co-authored-by: Esteban <estebandurangov@users.noreply.github.com>

--- reverse2.c
+++ reverse2.c
@@ -2,26 +2,61 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Creación de lista ligada
 typedef struct LineNode {
     char* line;
     struct LineNode* next;
 } LineNode;
 
-int main (int argc, char *argv[]){
-    FILE *file;
-    FILE *file2;
+//crear lista de lineas
+LineNode* crearLista(FILE *file){
+    size_t read;
     char *line = NULL;
     size_t len = 0;
-    size_t read;
 
     LineNode* head = NULL; 
-    
+
+    // Leer cada línea del archivo por medio de lista ligada
+    while ((read = getline(&line, &len, file)) != -1) {
+        LineNode* newNode = (LineNode*)malloc(sizeof(LineNode));
+        newNode->line = strdup(line);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    free(line);
+    fclose(file);
+
+    return head;
+}
+
+void imprimirLista(LineNode *head){
+    //Imprimir la lista ligada
+    while (head != NULL) {
+        printf("%s", head->line);
+        free(head->line);  // Liberar la memoria de la línea
+        LineNode* temp = head;
+        head = head->next;
+        free(temp);  // Liberar el nodo
+    }
+}
+
+
+int main (int argc, char *argv[]){
+    FILE *file;
+    FILE *file2;    
 
     //verificar que el usuario proporciona la cantidad correcta de archivos de entrada
     if (argc > 3){
         fprintf(stderr, "usage: reverse <input> <output>\n");             //--1--
         exit(1);
-    } else if (argc >= 2){
+    } 
+
+    else if (argc == 1){
+        //ingresar txt por consola
+    }
+    
+    else if (argc >= 2){
 
         // Intentar abrir el archivo de entrada             
         file = fopen(argv[1], "r");
@@ -29,10 +64,18 @@ int main (int argc, char *argv[]){
             fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);    //--2--
             exit(1);
         }
+        LineNode *head = crearLista(file);
+        if (argc == 2){
+            imprimirLista(head);
+        }
+
+        if (argc == 3){
+            //mover los casos para el caso 3 y mirar como vamos a imprimir por archivo
+        }
         
     } 
 
-    if (argc == 3){
+     if (argc == 3){
         //Intentar abrir archivo de salida
         file2 = fopen(argv[2], "r");
         if (file2 == NULL) {
@@ -66,12 +109,5 @@ int main (int argc, char *argv[]){
     }
 
 //-----------------------------------------------//
- 
-    // Leer y mostrar cada línea del archivo
-    while ((read = getline(&line, &len, file)) != -1) {
-        i++;
-        printf("%s", line);
-    }
-
     return 1;
 }
